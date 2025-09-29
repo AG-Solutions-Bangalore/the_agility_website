90397f008c5cb872449fc719a6989cf4198347df
Sajid-tech <s.khan9430319425@gmail.com>
2025-09-29 16:16:44 +0530

    `agility_web`

---
+++ src/components/layout/header/Navigation/Menudata.tsx
@@ -0,0 +1,12 @@
+import { HeaderItem } from "@/types/menu";
+
+
+export const headerData: HeaderItem[] = [
+    { label: 'About us', href: '/#aboutus' },
+    { label: 'Services', href: '/#services' },
+    { label: 'Work', href: '/#work' },
+    { label: 'Client', href: '/#client' },
+
+    
+    { label: 'Contact', href: '/contact' },
+];
